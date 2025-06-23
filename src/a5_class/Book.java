b7f509c30386dfc26ddae2cf2f7f1f41f28e2649
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-23 15:45:10 +0900

    Create Book.java

---
+++ src/a5_class/Book.java
@@ -0,0 +1,14 @@
+package a5_class;
+
+public class Book {
+    /*
+    필드 :
+    제목 (title)
+    저자 (author)
+    가격 (price)
+    생성자 : 기본생성자와 모든필드를 포함한 생성자
+    메서드 : 책의 정보를 출력하는 메서드
+    printBookInfo()
+    "제목 by 저자, 가격원" - 해리포터 by J.K.롤링, 14000원
+     */
+}
