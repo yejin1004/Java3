a25800eb6d0c2b97313a274a2ceb1096366e36b3
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-24 12:40:50 +0900

    update

--- src/a5_class/Constructor1.java
+++ src/a5_class/Constructor1.java
@@ -2,7 +2,13 @@ package a5_class;
 
 public class Constructor1 {
     public static void main(String[] args) {
-
+        A a = new A();
+        a. work();
+        B b = new B();
+        b.work();
+        //C c = new C(); // 에러. 기본생성자 없음
+        C c = new C(100); // 선언한 생성자를 반드시 사용해야 함!!
+        c.work();
     }
 }
 
@@ -12,14 +18,26 @@ public class Constructor1 {
 // public class A {} // 에러. public 사용하면안됨
 class A {
     int m;
+    // 생성자가 없음 => 생성자 선언이 없으면 자동으로 기본생성자를 만듬
     void work() {
         System.out.println(m);
     }
 }
 class B {
     int m;
-    B() {
+    B() { // 기본생성자를 명시적으로 선언했음. 자동으로 만들어지지 않음
         System.out.println("B의 기본생성자");
+    }
+    void work() {
+        System.out.println(m);
+    }
+}
+class C {
+    int m;
+    // 기본생성자가 없음. 하지만 다른 생성자가 있으므로 자동으로 기본생성자를
+    // 만들지 않음. C 클래스를 생성하려면 반드시 이 생성자를 사용해야 함!!
+    C(int a) {
+        m = a;
     }
     void work() {
         System.out.println(m);
