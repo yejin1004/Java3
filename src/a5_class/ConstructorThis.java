13e04b93bedae6389b6c9320a995786338ab901d
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-24 14:35:13 +0900

    update

--- src/a5_class/ConstructorThis.java
+++ src/a5_class/ConstructorThis.java
@@ -3,9 +3,10 @@ package a5_class;
 public class ConstructorThis {
     public static void main(String[] args) {
         AAA a = new AAA(3,5);
-        a.print();
+        a.print(); // 0 5
         // 클래스의 필드와 메서드의 지역변수가 같으면 필드를 지역변수로 인식하므로
         // 명시적으로 this를 사용하여 필드를 표시해야 함. (this.필드)
+
         BBB b1 = new BBB();
         b1.print(); // 1 2 3 4
         BBB b2 = new BBB(10);
@@ -18,7 +19,7 @@ class AAA {
     int m;
     int n;
     AAA(int m, int n) {
-        m = m;
+        m = m; // 대입연산자 왼쪽 m은 필드의 m이 아니라 생성자의 매개변수 m을 의미함
         this.n = n;
     }
     void print() {
@@ -37,7 +38,7 @@ class BBB {
         this.m4 = 4;
     }
     BBB(int m1) {
-        this();
+        this(); // this 메서드는 자신이 속한 클래스내부의 다른 생성자를 호출하는것
         this.m1 = m1;
     }
     BBB(int m1, int m2) {
---
+++ src/a6_array/Array1.java
@@ -0,0 +1,8 @@
+package a6_array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        // 배열 Array
+        // 자료구조 (Data Structure)
+    }
+}
