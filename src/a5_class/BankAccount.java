8e602d5d48afa79a945e5abc98f3ec0783797f50
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-25 14:36:11 +0900

    update

---
+++ src/a5_class/BankAccount.java
@@ -0,0 +1,13 @@
+package a5_class;
+
+public class BankAccount {
+    // 필드
+    // accountNumber 계좌번호
+    // ownerName 예금주 이름
+    // balance 잔액
+    // 생성자 (기본과 매개변수있는것)
+    // 메서드
+    // deposit(입금할 금액) : 돈을 입금하는 메서드 (양수만 가능)
+    // withdraw(출금할 금액) : 돈을 출금하는 메서드(양수만 가능. 잔액보다 출금액 많으면 경고표시)
+    // toString() : 계좌번호, 예금주이름, 잔액 화면출력
+}
