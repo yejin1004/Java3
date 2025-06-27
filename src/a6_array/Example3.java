b39dde8beab89951ab00c4d0896ec9702ed81772
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-27 10:29:24 +0900

    Create Example3.java

---
+++ src/a6_array/Example3.java
@@ -0,0 +1,23 @@
+package a6_array;
+
+public class Example3 {
+    public static void main(String[] args) {
+        // 중복된 숫자의 갯수를 리턴하는 메서드.
+        // 아래 배열에 3의 갯수는 3개이므로 3이 출력
+        // 없으면 0을 출력
+        int[] numbers = {1,3,7,2,4,3,9,8,3,4};
+        int count = countNumber(numbers, 3);
+        System.out.println(count); // 3
+        count = countNumber(numbers, 10);
+        System.out.println(count); // 0
+    }
+    public static int countNumber(int[] numbers, int target) {
+        int count = 0;
+        for (int data : numbers) {
+            if (data == target) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
