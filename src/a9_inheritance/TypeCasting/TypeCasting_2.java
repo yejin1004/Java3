41cb6d47fb37b797e8b716bc282c1745dfa1a61d
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-07-02 14:02:42 +0900

    update

--- src/a9_inheritance/TypeCasting/TypeCasting_2.java
+++ src/a9_inheritance/TypeCasting/TypeCasting_2.java
@@ -13,6 +13,9 @@ public class TypeCasting_2 {
         System.out.println(value2 instanceof D); // true
         // instanceof는 뒤에 적는 클래스의 특징(=속성)을
         // 가지고 있는지 물어보는 키워드
+        // 반환형은 반드시 Boolean (참거짓)
+
+        // 아래코드는 value1 객체를 D 타입으로 변환해도 되는지 여부를 확인하는데 사용
         if (value1 instanceof D) {
             D d = (D)value1;
             System.out.println("value1을 D로 다운캐스팅");
@@ -28,8 +31,9 @@ public class TypeCasting_2 {
         }
 
         // 객체의 실제 타입을 직접적으로 알 수 있는 방법??
-        System.out.println(value1.getClass().getName()); // TypeCasting.C
-        System.out.println(value2.getClass().getName()); // TypeCasting.D
+        // getName()의 결과는 패키지명.클래스명 형태 (풀네임)
+        System.out.println(value1.getClass().getName()); // a9_inheritance.TypeCasting.C
+        System.out.println(value2.getClass().getName()); // a9_inheritance.TypeCasting.D
 
         // instnaceof와 getClass.getName의 차이점
         // instanceof는 상속관계를 포함하고 있음
